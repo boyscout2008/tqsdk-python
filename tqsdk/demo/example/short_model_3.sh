1295cf576b48bb4d0ebcb03cfa0305cafe5c886a
boyscout2008 <tqdada@hotmail.com>
2020-04-05 13:21:38 +0800

    short_model_3 for zhendang

---
+++ tqsdk/demo/example/short_model_3.sh
@@ -0,0 +1,9 @@
+# 有效回测日期
+#先相对高
+#/d/Anaconda3/python short_model_3_huice.py --SYMBOL DCE.i2005 --YEAR 2019 --MONTH 12 --DAY 17 --YALIWEI 650 --LAST_CLOSE 645.5 --LAST_SETTLEMENT 652.5
+#震荡空
+#/d/Anaconda3/python short_model_3_huice.py --SYMBOL DCE.i2005 --YEAR 2020 --MONTH 1 --DAY 9 --YALIWEI 685 --LAST_CLOSE 679.5 --LAST_SETTLEMENT 677
+#/d/Anaconda3/python short_model_3_huice.py --SYMBOL DCE.i2005 --YEAR 2019 --MONTH 12 --DAY 12 --YALIWEI 662 --LAST_CLOSE 658.5 --LAST_SETTLEMENT 652
+/d/Anaconda3/python short_model_3_huice.py --SYMBOL DCE.i2005 --YEAR 2019 --MONTH 12 --DAY 16 --YALIWEI 669 --LAST_CLOSE 653.5 --LAST_SETTLEMENT 651
+#无效风控日期
+#/d/Anaconda3/python short_model_3.py --SYMBOL DCE.i2009 --YALIWEI 580 --LAST_CLOSE 577.5 --LAST_SETTLEMENT 560
